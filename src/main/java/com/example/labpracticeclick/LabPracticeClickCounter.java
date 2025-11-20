ab86a21130c3a5dd2ad02c87665f9d13a84df62e
gbc101579615 <genesis.tugawin@georgebrown.ca>
2025-11-20 07:56:42 -0500

    added form and click counter

--- src/main/java/com/example/labpracticeclick/LabPracticeClickCounter.java
+++ src/main/java/com/example/labpracticeclick/LabPracticeClickCounter.java
@@ -3,6 +3,9 @@ package com.example.labpracticeclick;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
